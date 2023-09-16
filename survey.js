33b034ea5c2b7a04edbc424f52028e7517aae55b
maddie <lee.maddie@gmail.com>
2023-09-15 23:59:55 -0400

    Inserted remaining survey questions and end paragraph

--- survey.js
+++ survey.js
@@ -13,8 +13,29 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => 
   rl.question('What\'s an activity you like doing? ', (answer) => {
     profile.activity = answer;
     
-    console.log("Profile:", profile);
-    
-    rl.close();
+    rl.question('What do you listen to while doing that? ', (answer) => {
+      profile.listen = answer;
+      
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+        profile.favMeal = answer;
+        
+        rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
+          profile.favFood = answer;
+          
+          rl.question('Which sport is your absolute favourite? ', (answer) => {
+            profile.favSport = answer;
+            
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+              profile.superpower = answer;
+
+              console.log(`\n${profile.name} loves listening to ${profile.listen} while ${profile.activity}, devouring ${profile.favFood} for ${profile.favMeal}, prefers ${profile.favSport} over any other sport, and is amazing at ${profile.superpower}.`);
+              
+              rl.close();
+        
+            });
+          });
+        });
+      });
+    });
   });
 });
